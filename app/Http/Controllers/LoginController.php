304fc561d79e33b99790b343ad1fe4b4e5f50604
JBeees <arifkizn9@gmail.com>
2025-04-30 19:07:17 +0700

    loginreg

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+use PDO;
+
+class LoginController extends Controller
+{
+    public function loginCheck(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:3'
+        ]);
+        $email = htmlspecialchars($request->input('email'));
+        $pass = htmlspecialchars($request->input('password'));
+        $cekEmail = DB::table('school_account')->where('email', $email)->first();
+        if (!$cekEmail) {
+            return back()->withErrors(['email' => 'Email not registered']);
+        }
+        if (Hash::check($pass, $cekEmail->password)) {
+            session(['user' => $cekEmail]);
+            return redirect()->route('loggedIn');
+        } else {
+            return back()->withErrors(['password' => 'Incorrect password']);
+        }
+    }
+}
--- resources/views/nextRegistrasi.blade.php
+++ resources/views/nextRegistrasi.blade.php
@@ -86,7 +86,7 @@
         }
 
         button:hover {
-            background-color: darkblue;
+            background-color: black;
             transform: scale(1.05);
             box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2);
         }
@@ -104,6 +104,36 @@
             pointer-events: none;
             opacity: 30%;
         }
+
+        .red {
+            color: red;
+        }
+
+        #errorPopUp {
+            display: none;
+            position: fixed;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background-color: rgba(0, 0, 0, 0.5);
+            justify-content: center;
+            align-items: center;
+            z-index: 9999;
+        }
+
+        #popupContent {
+            background-color: white;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+            text-align: center;
+            width: 300px;
+        }
+
+        #errorButton {
+            width: 100px;
+        }
     </style>
 </head>
 
@@ -112,43 +142,44 @@
         <div class="login">
             <a href="{{ route('dashboard') }}" class="nusa">NusaFood</a>
             <h2 class="regis">Register</h2>
-            <form class="form">
+            <form class="form" action="{{ route('temp-store2') }}" method="post">
+                @csrf
                 <div class="loginForm">
-                    <label>Nama Sekolah</label>
-                    <input style="width:600px;height:40px;font-size:20px" type="type" placeholder="Nama Sekolah">
+                    <label>Nama Sekolah<span class="red">*</span></label>
+                    <input style="width:600px;height:40px;font-size:20px" type="text" name="name" placeholder="Nama Sekolah">
                 </div>
                 <div class="loginForm">
                     <div style="display:flex;flex-direction:row;gap:10px">
                         <div class="loginForm">
-                            <label>No. Telepon</label>
-                            <input style="width:220px;height:40px;font-size:20px" type="tel" placeholder="123-456-789">
+                            <label>No. Telepon<span class="red">*</span></label>
+                            <input style="width:220px;height:40px;font-size:20px" type="tel" name="phone" placeholder="123-456-789">
                         </div>
                         <div class="loginForm">
-                            <label>Alamat</label>
-                            <input style="width:370px;height:40px;font-size:20px" type="tel" placeholder="Jl Ciamis">
+                            <label>Alamat<span class="red">*</span></label>
+                            <input style="width:370px;height:40px;font-size:20px" type="text" name="address" placeholder="Jl Ciamis">
                         </div>
                     </div>
                 </div>
                 <div class="loginForm">
                     <div class="daerah" style="display:flex;flex-direction:row; gap:10px">
                         <div style="display:flex;flex-direction:column;">
-                            <label>Banyak Siswa</label>
-                            <input style="width:120px;height:40px;font-size:20px;text-align:center;" type="number"
+                            <label>Banyak Siswa<span class="red">*</span></label>
+                            <input style="width:120px;height:40px;font-size:20px;text-align:center;" name="student" type="number"
                                 placeholder="100">
                         </div>
                         <div style="display:flex;flex-direction:column;">
-                            <label>Provinsi</label>
+                            <label>Provinsi<span class="red">*</span></label>
                             <select
-                                style="width:230px;height:40px;font-size:20px;border-radius: 20px;padding-left: 12px;background:none"
-                                id="province">
+                                style="width:225px;height:40px;font-size:20px;border-radius: 20px;padding-left: 12px;background:none"
+                                id="province" name="province">
                                 <option disabled selected>Memuat provinsi…</option>
                             </select>
                         </div>
                         <div style="display:flex;flex-direction:column;">
-                            <label>Kabupaten/Kota</label>
+                            <label>Kabupaten/Kota<span class="red">*</span></label>
                             <select
-                                style="width:230px;height:40px;font-size:20px;border-radius: 20px;padding-left: 12px;background:none"
-                                id="city" disabled>
+                                style="width:225px;height:40px;font-size:20px;border-radius: 20px;padding-left: 12px;background:none"
+                                id="city" name="city" disabled>
                                 <option disabled selected>—</option>
                             </select>
                         </div>
@@ -156,8 +187,7 @@
                 </div>
                 <div class="buttons">
                     <button type="button" onclick="window.location.href='{{ route('registrasi') }}'">Kembali</button>
-                    <button type="button" onclick="window.location.href='{{ route('login') }}'"
-                        class="masuk">Submit</button>
+                    <button type="submit" class="masuk">Submit</button>
                 </div>
             </form>
         </div>
@@ -166,6 +196,25 @@
             <img class="map" src="{{ asset('Image/indo_bg.png') }}">
         </div>
     </div>
+    @if ($errors->any() || session('error'))
+        <div id="errorPopUp">
+            <div id="popupContent">
+                @foreach ($errors->all() as $error)
+                    <p>{{ $error }}</p>
+                @endforeach
+                <p>{{ session('error') }}</p>
+                <button id="errorButton" onclick="closePopup()">Close</button>
+            </div>
+        </div>
+
+        <script>
+            var myModal = document.getElementById('errorPopUp');
+            myModal.style.display = 'flex';
+            function closePopup() {
+                myModal.style.display = 'none';
+            }
+        </script>
+    @endif
     <script>
         const URL_PROVINCES = 'https://raw.githubusercontent.com/Caknoooo/provinces-cities-indonesia/main/json/provinces.json';
         const URL_REGENCIES = 'https://raw.githubusercontent.com/Caknoooo/provinces-cities-indonesia/main/json/regencies.json';
@@ -203,8 +252,7 @@
                 citySelect.disabled = false;
             });
         }
-
-        loadDaerah().catch(err => console.error(err));
+        document.addEventListener('DOMContentLoaded', loadDaerah);
     </script>
 </body>
 
